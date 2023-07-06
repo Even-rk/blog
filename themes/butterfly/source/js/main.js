77d411f5b2a737630a5470061609257fe5c7b287
Aeary <2921401460@qq.com>
2023-07-06 10:15:17 +0800

    小bug处理

--- themes/butterfly/source/js/main.js
+++ themes/butterfly/source/js/main.js
@@ -533,7 +533,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (isExpand) return;
         let parent = currentActive.parentNode;
 
-        for (; !parent.matches(".toc"); parent = parent.parentNode) {
+        for (; !parent?.matches(".toc"); parent = parent.parentNode) {
           if (parent.matches("li")) parent.classList.add("active");
         }
       }
